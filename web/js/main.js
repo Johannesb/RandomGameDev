d1a17452425a962e016417580508a902200d870d
Johannesb <joberthel@msn.com>
2016-11-28 15:40:15 +0100

    test

--- web/js/main.js
+++ web/js/main.js
@@ -2,10 +2,13 @@
 requirejs.config({
   urlArgs: "bust=" + Math.random(),
   baseUrl: 'js',
-  paths:   {
+
+  paths: {
     // paths to app + json + lib folders
     app:  '../app',
     json: '../json',
+
+    tdge: 'lib/tdge',
 
     // dependency to load json files with requirejs
     text:     'lib/require/text',
@@ -14,15 +17,32 @@ requirejs.config({
 });
 
 // main function
-requirejs(['loadjson!json/app.json'],
-  function (App)
+requirejs(['loadjson!json/app.json', 'tdge/tdge', 'lib/vector2d'],
+  function (App, TDGE, Vector2D)
   {
-    App.paths.forEach(function (path)
+    /*App.paths.forEach(function (path)
     {
       var a       = document.createElement('a');
       a.innerHTML = path;
       a.href      = 'app/' + path;
       document.body.appendChild(a);
+    });*/
+
+    var game = new TDGE.Game(800, 600, TDGE.Renderer.Canvas, {backgroundColor: '#dfdfdf'});
+
+    var obj = TDGE.Object.create(TDGE.Object.Rectangle, {
+      position:     new Vector2D([100, 75]),
+      anchor:       {x: .5, y: .5},
+      speed:        new Vector2D([150, 0]),
+      acceleration: new Vector2D([0, 100]),
+      width:  60,
+      height: 35,
+      color:  '#ff0000'
     });
+    game.appendChild(obj);
+
+    document.body.appendChild(game.renderer.view);
+
+    game.start();
   }
 );
